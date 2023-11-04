5fc7d469942dbe55a4de108463d83f43fa425d9b
John B <johan@nexys.ch>
2023-11-04 14:03:55 +0100

    Update index.ts

--- src/rest/index.ts
+++ src/rest/index.ts
@@ -4,4 +4,4 @@ export * as Org from "./org";
 export * as Repo from "./repo";
 export * as Utils from "./utils";
 export * as Validity from "./validity";
-export * as Workflow from './workflow';
+export * as Workflow from './workflows';
